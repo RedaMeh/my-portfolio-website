c14c843f3e6fbbb7b85d8fd60666e3f4d22b6751
redaMeh <mohamed.mehdaoui@epfl.ch>
2025-01-04 17:23:59 -0600

    Added tennis serve prep video and preliminary theater curtains to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,26 +2,35 @@
 
 import { motion } from "framer-motion";
 import Link from 'next/link';
-import tennisImage from '../assets/images/yanBerthemy.jpg';
+import './Curtain.css';
 
 export default function Home() {
   return (
-    <motion.div
-      className="flex h-screen flex-col items-center space-y-4 bg-cover bg-center"
-      style={{ backgroundImage: `url(${tennisImage.src})` }}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 3 }}
-    >
-      <h1 className="text-4xl font-bold text-black">Welcome to My Portfolio!</h1>
-      
-      <div className="p-8 bg-gradient-to-r from-blue-500 to-green-500 text-white rounded-lg">
-        Tailwind CSS is up and ready to use!
-      </div>
+    <div className="relative h-screen w-screen overflow-hidden">
+      <div className="curtain left"></div>
+      <div className="curtain right"></div>
+      <video
+        className="absolute top-0 left-0 w-full h-full object-cover"
+        src="/videos/ServePrep.mp4" // Use the public path
+        autoPlay
+        muted
+      />
+      <motion.div
+        className="relative flex h-screen flex-col items-start space-y-8 pl-8"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 5 }}
+      >
+        <h1 className="mt-8 text-4xl font-bold text-black">Welcome to My Portfolio!</h1>
+        
+        <div className="p-8 bg-gradient-to-r from-blue-500 to-green-500 text-white rounded-lg">
+          Tailwind CSS is up and ready to use!
+        </div>
 
-      <Link href="/contact" className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700 transition duration-300">
-        Contact Me
-      </Link>
-    </motion.div>
+        <Link href="/contact" className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700 transition duration-300">
+          Contact Me
+        </Link>
+      </motion.div>
+    </div>
   );
 }
