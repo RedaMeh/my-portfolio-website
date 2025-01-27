919e6f7c27f95327241d143e2d74f379c9ed7f1c
redaMeh <mohamed.mehdaoui@epfl.ch>
2025-01-27 15:11:04 -0600

    Reimagined projects page video animation because of Chrome

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,6 +4,10 @@ import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import './Projects.css';
 import Link from 'next/link';
+import Image from 'next/image';
+import backgroundImage from '@/assets/images/Guitar.png';
+import { motion } from 'framer-motion';
+
 
 const ProjectsPage: React.FC = () => {
     const [selectedProject, setSelectedProject] = useState<string | null>(null);
@@ -12,26 +16,37 @@ const ProjectsPage: React.FC = () => {
     const handleOptionClick = (project: string) => {
         setSelectedProject(project);
         router.prefetch(`/projects/${project}`);
+        playTransitionVideo(`/projects/${project}`);
     };
 
-    const handleVideoEnd = () => {
-        if (selectedProject) {
-            router.push(`/projects/${selectedProject}`);
-        }
-    };
+    function playTransitionVideo(href: string) {
+        const videoElement = document.createElement('video');
+        videoElement.src = '/video/Guitar.mp4';
+        videoElement.className = 'absolute top-0 left-0 w-full h-full object-cover z-[-1]';
+        videoElement.autoplay = true;
+        videoElement.muted = true;
+        videoElement.onended = () => {
+          window.location.href = href;
+        };
+        document.body.appendChild(videoElement);
+      }
+
+
 
     const handleGoBackHome = () => {
         router.push('/');
     };
 
     return (
-        <div className="projects-page">
-            <video
-                className="absolute top-0 left-0 w-full h-full object-cover"
-                src="/video/Guitar.mp4"
-                autoPlay={!!selectedProject}
-                muted
-                onEnded={handleVideoEnd}
+        <motion.div className="projects-page">
+            
+            <Image
+                src={backgroundImage}
+                alt="Background"
+                layout="fill"
+                objectFit="cover"
+                quality={100}
+                className="z-[-1]"
             />
             <div className="top-left-text">Choose a project</div>
             <div>
@@ -46,7 +61,7 @@ const ProjectsPage: React.FC = () => {
                 Guitar Stock Videos by Vecteezy
             </Link>
             <button className="go-back-home" onClick={handleGoBackHome}>Back Home</button>
-        </div>
+        </motion.div>
         
     );
 };
